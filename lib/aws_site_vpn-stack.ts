a8b6ab53a219098981f5011851baf23d4e6e8286
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-12-01 22:41:07 +0900

    add vpc with vpn setting

--- lib/aws_site_vpn-stack.ts
+++ lib/aws_site_vpn-stack.ts
@@ -1,9 +1,38 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Vpc, SubnetType } from "@aws-cdk/aws-ec2";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "/Users/sakurairyou/Desktop/program/aws_site_vpn/.env" });
 
 export class AwsSiteVpnStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        if (!process.env.HOME_IP || !process.env.HOME_CIDR)
+            throw new Error("Env error");
+
+        new Vpc(this, "vpn_vpc", {
+            cidr: "10.0.0.0/16",
+            maxAzs: 1,
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: "private",
+                    subnetType: SubnetType.PRIVATE_WITH_NAT,
+                },
+                {
+                    cidrMask: 24,
+                    name: "public",
+                    subnetType: SubnetType.PUBLIC,
+                },
+            ],
+            vpnGateway: true,
+            vpnConnections: {
+                homeVPN: {
+                    ip: process.env.HOME_IP!,
+                    staticRoutes: [process.env.HOME_CIDR!],
+                },
+            },
+        });
+    }
 }
